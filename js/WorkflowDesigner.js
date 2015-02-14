f921ef41b2141eed1e05fc89a62683b46c303253
Rajitha Wimalasooriya <rajithaw@hotmail.com>
2015-02-15 08:07:10 +1100

    Render workflow item connections

--- js/WorkflowDesigner.js
+++ js/WorkflowDesigner.js
@@ -7,13 +7,42 @@ function WorkflowDesigner(width, height, container, items){
         });
 
     var render = function() {
-        svg.selectAll("circle")
-            .data(itemsCollection.toArray())
-            .enter().append("circle")
-            .attr("r", 10)
-            .attr("cx", function(d) { return (d.x * 100) + 20; })
-            .attr("cy", function(d) { return (d.y * 100) + 20; })
-            .exit().remove();
+        var magnitude = 100,
+            offset = 20,
+            connectorData = [];
+
+        // Render the nodes
+        var itemNodes = svg.selectAll("circle")
+            .data(itemsCollection.toArray());
+
+        itemNodes.enter().append("circle");
+        itemNodes.exit().remove();
+        itemNodes.attr("r", 10)
+            .attr("cx", function(d) { return (d.x * magnitude) + offset; })
+            .attr("cy", function(d) { return (d.y * magnitude) + offset; });
+
+        // work through all items higher than level 0
+        itemNodes.filter(function(d) { return d.level > 0; })
+            .each(function(d) {
+                var previousLevel = itemsCollection[d.level -1];
+
+                // Construct the connector data using the nodes to be connected
+                for(var i = 0, j = previousLevelLength = previousLevel.length; i < previousLevelLength; i++) {
+                    connectorData.push(new WorkflowItemConnector(previousLevel[i], d));
+                }
+            });
+
+        // Render the connections
+        var connectors = svg.selectAll("line")
+            .data(connectorData);
+        connectors.enter().append("line");
+        connectors.exit().remove();
+
+        connectors.attr("x1", function(d) { return (d.n1.x * magnitude) + offset; })
+            .attr("y1", function(d) { return (d.n1.y * magnitude) + offset; })
+            .attr("x2", function(d) { return (d.n2.x * magnitude) + offset; })
+            .attr("y2", function(d) { return (d.n2.y * magnitude) + offset; })
+            .attr("stroke", "black");
     };
 
     return {
---
+++ js/model/WorkflowItemConnector.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Rajitha on 2/15/2015.
+ */
+
+function WorkflowItemConnector(item1, item2) {
+    return {
+        workflowItem1: item1,
+        workflowItem2: item2
+    }
+}
