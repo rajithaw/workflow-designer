fb05db74c9a2d6bd009b56111bff57018ede0459
Rajitha Wimalasooriya <rajithaw@hotmail.com>
2015-02-14 19:52:23 +1100

    Add files required to display workflow items

--- js/WorkflowDesigner.js
+++ js/WorkflowDesigner.js
@@ -1,7 +1,34 @@
-function WorkflowDesigner(){
-    var itemsCollection;
+function WorkflowDesigner(width, height, container, items){
+    var itemsCollection = items;
+    var svg = container.append("svg")
+        .attr({
+            width: width,
+            height: height
+        });
 
-    var Render = function() {
+    var render = function() {
+        var item;
 
+        for(var i = 0; i < itemsCollection.length; i++)
+        {
+            for(var j = 0; j < itemsCollection[i].length; j++)
+            {
+                item = itemsCollection[i][j];
+
+                svg.append("rect")
+                    .attr({
+                        x: (item.x * 100) + 20,
+                        y: (item.y * 100) + 20,
+                        rx: 5,
+                        ry: 5,
+                        width: 10,
+                        height: 10
+                    })
+            }
+        }
+    };
+
+    return {
+        render: render
     };
 }
