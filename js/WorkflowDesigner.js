ff943ba625317134f94e255346650a46698ba182
Rajitha Wimalasooriya <rajithaw@hotmail.com>
2015-02-15 11:38:49 +1100

    Modify item appearences

--- js/WorkflowDesigner.js
+++ js/WorkflowDesigner.js
@@ -9,17 +9,32 @@ function WorkflowDesigner(width, height, container, items){
     var render = function() {
         var magnitude = 100,
             offset = 20,
-            connectorData = [];
+            connectorData = [],
+            itemWidth = 40,
+            itemHeight = 20,
+            intermediateSize = 10;
 
         // Render the nodes
-        var itemNodes = svg.selectAll("circle")
+        var itemNodes = svg.selectAll("rect")
             .data(itemsCollection.toArray());
 
-        itemNodes.enter().append("circle");
+        itemNodes.enter().append("rect");
         itemNodes.exit().remove();
-        itemNodes.attr("r", 10)
-            .attr("cx", function(d) { return (d.x * magnitude) + offset; })
-            .attr("cy", function(d) { return (d.y * magnitude) + offset; });
+        itemNodes.attr("width", function(d){ return d.id === -1 ? intermediateSize : itemWidth })
+            .attr("height", function(d) { return d.id === -1 ? intermediateSize : itemHeight })
+            .attr("rx", function(d) { return 5; })
+            .attr("ry", function(d) { return 5; })
+            .attr("fill", function(d) { return d.id === -1 ? "black" : "blue" })
+            .attr("x", function(d) {
+                return d.id === -1
+                    ? (d.x * magnitude) + offset - (intermediateSize / 2)
+                    : (d.x * magnitude) + offset - (itemWidth / 2);
+            })
+            .attr("y", function(d) {
+                return d.id === -1
+                    ? (d.y * magnitude) + offset - (intermediateSize / 2)
+                    : (d.y * magnitude) + offset - (itemHeight / 2);
+            });
 
         // work through all items higher than level 0
         itemNodes.filter(function(d) { return d.level > 0; })
@@ -38,9 +53,18 @@ function WorkflowDesigner(width, height, container, items){
         connectors.enter().append("line");
         connectors.exit().remove();
 
-        connectors.attr("x1", function(d) { return (d.workflowItem1.x * magnitude) + offset; })
+        connectors
+            .attr("x1", function(d) {
+                return d.workflowItem1.id === -1
+                    ? (d.workflowItem1.x * magnitude) + offset
+                    : (d.workflowItem1.x * magnitude) + offset + (itemWidth / 2);
+            })
             .attr("y1", function(d) { return (d.workflowItem1.y * magnitude) + offset; })
-            .attr("x2", function(d) { return (d.workflowItem2.x * magnitude) + offset; })
+            .attr("x2", function(d) {
+                return d.workflowItem2.id === -1
+                    ? (d.workflowItem2.x * magnitude) + offset
+                    : (d.workflowItem2.x * magnitude) + offset - (itemWidth / 2);
+            })
             .attr("y2", function(d) { return (d.workflowItem2.y * magnitude) + offset; })
             .attr("stroke", "black");
     };
