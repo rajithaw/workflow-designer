b32b3a3fa7a639f79beca94beaaffe1e0d6d69ca
Rajitha Wimalasooriya <rajithaw@hotmail.com>
2015-05-18 19:07:14 +1000

    Add workflow controller implementation

--- app/app.js
+++ app/app.js
@@ -2,14 +2,14 @@
  * Created by Rajitha on 5/17/2015.
  */
 
-var demoApp = angular.module("demoApp", ["ngRoute"]);
+var demoApp = angular.module("demoApp", ["ngRoute", "ngResource"]);
 
 demoApp.config(function ($routeProvider) {
     $routeProvider
         .when('/workflows',
         {
             controller: 'WorkflowsController',
-            templateUrl: 'app/partials/listWorkflows.html'
+            templateUrl: 'app/partials/workflowsList.html'
         })
         .when('/workflows/:workflowId',
         {
--- app/controllers/workflowsController.js
+++ app/controllers/workflowsController.js
@@ -2,6 +2,35 @@
  * Created by Rajitha on 5/17/2015.
  */
 
-demoApp.controller("WorkflowsController", function($scope) {
+demoApp.controller("WorkflowsController", function($scope, $http) {
 
+    var init = function() {
+        initWorkflows();
+    };
+
+    var initWorkflows = function() {
+        if(!$scope.workflowList){
+            getWorkflows();
+        }
+    };
+
+    var getWorkflows = function() {
+        $http.get("app/data/sampleWorkflows.json").success(function(data) {
+            $scope.workflowList = data;
+        });
+    };
+
+    $scope.deleteWorkflow = function(workflowId){
+        var i;
+
+        for(i = 0; i < $scope.workflowList.length; i++) {
+            if($scope.workflowList[i].id == workflowId) {
+                break;
+            }
+        }
+
+        $scope.workflowList.splice(i, 1);
+    };
+
+    init();
 });
