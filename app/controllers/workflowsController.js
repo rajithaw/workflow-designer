9dfff1e13dc9265309c2d42a6d72ac0b3dc63b78
rajithaw <rajithaw@hotmail.com>
2015-05-20 14:11:20 +1000

    Add edit workflow page controls

--- app/controllers/workflowsController.js
+++ app/controllers/workflowsController.js
@@ -2,12 +2,18 @@
  * Created by Rajitha on 5/17/2015.
  */
 
-demoApp.controller("WorkflowsController", function($scope, $http, workflowService) {
+demoApp.controller("WorkflowsController", function($scope, $http, $routeParams, workflowService) {
 
-    var initialize = function() {
-        workflowService.getWorkflows().then(function(workflows){
-            $scope.workflowList = workflows;
-        });
+    var initialize = function () {
+        if ($routeParams.workflowId) {
+            workflowService.getWorkflow($routeParams.workflowId).then(function (workflow) {
+
+            });
+        } else {
+            workflowService.getWorkflows().then(function (workflows) {
+                $scope.workflowList = workflows;
+            });
+        }
     };
 
     $scope.getWorkflows = function() {
@@ -16,10 +22,18 @@ demoApp.controller("WorkflowsController", function($scope, $http, workflowServic
         });
     };
 
-    $scope.deleteWorkflow = function(workflowId){
-        workflowService.deleteWorkflow(workflowId).then(function(){
+    $scope.deleteWorkflow = function(workflowId) {
+        workflowService.deleteWorkflow(workflowId).then(function() {
             $scope.getWorkflows();
-        })
+        });
+    };
+
+    $scope.onAddNodeClicked = function() {
+
+    };
+
+    $scope.onAddNodeClicked = function () {
+
     };
 
     initialize();
