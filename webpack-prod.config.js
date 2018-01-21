7a23cc014a2b396d54519d66c2421b5b6083bc15
Rajitha <rajithaw@hotmail.com>
2018-01-21 23:54:59 +1100

    Update output files

--- webpack-prod.config.js
+++ webpack-prod.config.js
@@ -3,11 +3,12 @@ var config = require('./webpack.config');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const extractSass = new ExtractTextPlugin({
-    filename: "workflow-designer.min.css"
+    filename: "index.css"
 });
 
 config.devtool = '';
-config.output.filename = 'workflow-designer.min.js';
+config.output.filename = 'index.js';
+
 config.plugins[0] = extractSass;
 
 module.exports = config;
